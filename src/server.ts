f11bb9cc7e400af6bd853c21e1479b3fb4dcf3c5
jamesjc78 <jla@noaya.no>
2022-03-09 08:47:25 +0800

    set access to any

--- src/server.ts
+++ src/server.ts
@@ -22,13 +22,10 @@ connectToDatabase()
       // set the CORS policy
       res.header("Access-Control-Allow-Origin", "*");
       // set the CORS headers
-      res.header(
-        "Access-Control-Allow-Headers",
-        "origin, X-Requested-With,Content-Type,Accept, Authorization"
-      );
+      res.header("Access-Control-Allow-Headers", "*");
       // set the CORS method headers
       if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+        res.header("Access-Control-Allow-Methods", "*");
         return res.status(200).json({});
       }
       next();
